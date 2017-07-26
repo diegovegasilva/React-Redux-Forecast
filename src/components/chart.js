2e6db1e96bf00c34e66a76ac455d6d12e1fd9a6c
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-26 13:48:15 +0200

    video 67

--- src/components/chart.js
+++ src/components/chart.js
@@ -10,7 +10,7 @@ function average(data) {
 export default (props) => {
     return (
         <div>
-            <Sparklines height={30} width={80} data={props.data}>
+            <Sparklines height={120} width={80} data={props.data}>
                 <SparklinesLine color={props.color} />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
